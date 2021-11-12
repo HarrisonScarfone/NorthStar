d89286f5ca02aa4312d0c67e7a9693815fe6b8ed
HarrisonScarfone <hscarfone77@gmail.com>
2021-11-11 22:32:21 -0500

    update

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello Workflow Testing!\n";
+    // std::cout << BuildSetupTest::addTwoInts(2,2) << "\n";
+
+    return 0;
+}
---
+++ src/somefunctions.cpp
@@ -0,0 +1,6 @@
+#include "somefunctions.h"
+
+int SomeFunctions::addTwoInts(int a, int b)
+{
+    return a + b;
+}
