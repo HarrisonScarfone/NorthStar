1932203ef44543b6d442edde0a3a552071de7e16
HarrisonScarfone <hscarfone77@gmail.com>
2021-11-21 20:44:56 -0500

    handle a position init from a fen string

--- src/gamestate.cpp
+++ src/gamestate.cpp
@@ -1,11 +1,17 @@
 #include "gamestate.h"
 
+#include <cstdint>
 #include <iostream>
+#include <string>
+#include <vector>
+
+#include "constants.h"
+#include "utilities.h"
 
 namespace Gamestate {
 
 namespace Initializers {
-    Position InitNewGameState() {
+    Position StartingPosition() {
         Position position;
         position.white_pawns = 71776119061217280U;
         position.white_rooks = 9295429630892703744U;
@@ -21,12 +27,121 @@ namespace Initializers {
         position.black_queens = 8U;
         position.black_king = 16U;
 
+        position.white_to_play = true;
+
+        position.can_black_castle_kingside = true;
+        position.can_black_castle_queenside = true;
+        position.can_white_castle_kindside = true;
+        position.can_white_castle_queenside = true;
+
+        position.halfmove_clock = 0;
+        position.fullmove_clock = 1;
+
+        return position;
+    }
+
+    Position PositionFromFenString(std::string fen_string) {
+        Position position;
+        std::vector<std::string> fen_tokens = Utilities::StringHandlers::TokenizeByDelimiter(fen_string, " ");
+
+        uint64_t location = 1;
+        // first token shows the piece positions so set them in the Gamestate::Position
+        for (auto character : fen_tokens.at(0)) {
+            switch (character) {
+                case '/':
+                    continue;
+                case 'P':
+                    position.white_pawns = position.white_pawns | location;
+                    break;
+                case 'R':
+                    position.white_rooks = position.white_rooks | location;
+                    break;
+                case 'N':
+                    position.white_knights = position.white_knights | location;
+                    break;
+                case 'B':
+                    position.white_bishops = position.white_bishops | location;
+                    break;
+                case 'Q':
+                    position.white_queens = position.white_queens | location;
+                    break;
+                case 'K':
+                    position.white_king = position.white_king | location;
+                    break;
+                case 'p':
+                    position.black_pawns = position.black_pawns | location;
+                    break;
+                case 'r':
+                    position.black_rooks = position.black_rooks | location;
+                    break;
+                case 'n':
+                    position.black_knights = position.black_knights | location;
+                    break;
+                case 'b':
+                    position.black_bishops = position.black_bishops | location;
+                    break;
+                case 'q':
+                    position.black_queens = position.black_queens | location;
+                    break;
+                case 'k':
+                    position.black_king = position.black_king | location;
+                    break;
+                default:
+                    // 0 is 48 in char
+                    // shift by 1 less than char val to account for loop shift
+                    location = location << (character - 49);
+                    break;
+            }
+            location = location << 1;
+        }
+
+        // second token shows "w" for white turn
+        position.white_to_play = fen_tokens.at(1) == "w";
+
+        // third token gives castling rights and - denotes no castling rights
+        for (auto character : fen_tokens.at(2)) {
+            switch (character) {
+                case 'K':
+                    position.can_white_castle_kindside = true;
+                    break;
+                case 'Q':
+                    position.can_white_castle_queenside = true;
+                    break;
+                case 'k':
+                    position.can_black_castle_kingside = true;
+                    break;
+                case 'q':
+                    position.can_black_castle_queenside = true;
+                    break;
+                default:
+                    continue;
+            }
+        }
+
+        // fourth token handles en passant rights
+        // set our enpassant board to high in the location given in the token
+        // example: a1 -> sets location 0 to high (0 - 63)
+        std::string enpassant_location = fen_tokens.at(3);
+
+        if (enpassant_location != "-") {
+            int enpassant_location_as_int = (enpassant_location[0] - 97) + ((8 - (enpassant_location[1] - 48)) * 8);
+            position.enpassant = Constants::int_to_uint.at(enpassant_location_as_int);
+        } else {
+            position.enpassant = 0;
+        }
+
+        // 5th token is the halfmove clock
+        position.halfmove_clock = std::stoi(fen_tokens.at(4));
+
+        // 6th token is the fullmove number, starts at 1
+        position.fullmove_clock = std::stoi(fen_tokens.at(5));
+
         return position;
     }
 }  // namespace Initializers
 
 namespace Output {
-    void PrintGamestate(Position position) {
+    void PrintPosition(Position position) {
         char piece_locations[64]{};
         uint64_t binary_position;
 
@@ -34,56 +149,56 @@ namespace Output {
             binary_position = 1;
             binary_position = binary_position << i;
 
-            if (Helpers::HasPieceAtBinaryLocation(position.white_pawns, binary_position)) {
+            if (Helpers::PieceUint64HasPieceAtLocation(position.white_pawns, binary_position)) {
                 piece_locations[i] = 'P';
             }
 
-            if (Helpers::HasPieceAtBinaryLocation(position.white_rooks, binary_position)) {
+            if (Helpers::PieceUint64HasPieceAtLocation(position.white_rooks, binary_position)) {
                 piece_locations[i] = 'R';
             }
 
-            if (Helpers::HasPieceAtBinaryLocation(position.white_knights, binary_position)) {
+            if (Helpers::PieceUint64HasPieceAtLocation(position.white_knights, binary_position)) {
                 piece_locations[i] = 'N';
             }
 
-            if (Helpers::HasPieceAtBinaryLocation(position.white_bishops, binary_position)) {
+            if (Helpers::PieceUint64HasPieceAtLocation(position.white_bishops, binary_position)) {
                 piece_locations[i] = 'B';
             }
 
-            if (Helpers::HasPieceAtBinaryLocation(position.white_queens, binary_position)) {
+            if (Helpers::PieceUint64HasPieceAtLocation(position.white_queens, binary_position)) {
                 piece_locations[i] = 'Q';
             }
 
-            if (Helpers::HasPieceAtBinaryLocation(position.white_king, binary_position)) {
+            if (Helpers::PieceUint64HasPieceAtLocation(position.white_king, binary_position)) {
                 piece_locations[i] = 'K';
             }
 
-            if (Helpers::HasPieceAtBinaryLocation(position.black_pawns, binary_position)) {
+            if (Helpers::PieceUint64HasPieceAtLocation(position.black_pawns, binary_position)) {
                 piece_locations[i] = 'p';
             }
 
-            if (Helpers::HasPieceAtBinaryLocation(position.black_rooks, binary_position)) {
+            if (Helpers::PieceUint64HasPieceAtLocation(position.black_rooks, binary_position)) {
                 piece_locations[i] = 'r';
             }
 
-            if (Helpers::HasPieceAtBinaryLocation(position.black_knights, binary_position)) {
+            if (Helpers::PieceUint64HasPieceAtLocation(position.black_knights, binary_position)) {
                 piece_locations[i] = 'n';
             }
 
-            if (Helpers::HasPieceAtBinaryLocation(position.black_bishops, binary_position)) {
+            if (Helpers::PieceUint64HasPieceAtLocation(position.black_bishops, binary_position)) {
                 piece_locations[i] = 'b';
             }
 
-            if (Helpers::HasPieceAtBinaryLocation(position.black_queens, binary_position)) {
+            if (Helpers::PieceUint64HasPieceAtLocation(position.black_queens, binary_position)) {
                 piece_locations[i] = 'q';
             }
 
-            if (Helpers::HasPieceAtBinaryLocation(position.black_king, binary_position)) {
+            if (Helpers::PieceUint64HasPieceAtLocation(position.black_king, binary_position)) {
                 piece_locations[i] = 'k';
             }
         }
 
-        std::cout << (position.whiteToPlay ? "White" : "Black") << " to play.\n";
+        std::cout << (position.white_to_play ? "White" : "Black") << " to play.\n";
         std::cout << "  ---------------------------------\n";
 
         for (int i = 0; i < 8; i++) {
@@ -104,8 +219,8 @@ namespace Output {
 }  // namespace Output
 
 namespace Helpers {
-    bool HasPieceAtBinaryLocation(uint64_t binary_location, uint64_t binary_position) {
-        return (binary_position & binary_location) > 0;
+    bool PieceUint64HasPieceAtLocation(uint64_t piece_64bit_locations, uint64_t location_to_check) {
+        return (piece_64bit_locations & location_to_check) > 0;
     }
 }  // namespace Helpers
 
--- src/gamestate.h
+++ src/gamestate.h
@@ -2,41 +2,46 @@
 #define GAMESTATE_H
 
 #include <cstdint>
+#include <string>
 
 namespace Gamestate {
 struct Position {
-    uint64_t white_pawns;
-    uint64_t white_rooks;
-    uint64_t white_knights;
-    uint64_t white_bishops;
-    uint64_t white_queens;
-    uint64_t white_king;
-    uint64_t black_pawns;
-    uint64_t black_rooks;
-    uint64_t black_knights;
-    uint64_t black_bishops;
-    uint64_t black_queens;
-    uint64_t black_king;
-    uint64_t enpassant;
+    uint64_t white_pawns = 0;
+    uint64_t white_rooks = 0;
+    uint64_t white_knights = 0;
+    uint64_t white_bishops = 0;
+    uint64_t white_queens = 0;
+    uint64_t white_king = 0;
+    uint64_t black_pawns = 0;
+    uint64_t black_rooks = 0;
+    uint64_t black_knights = 0;
+    uint64_t black_bishops = 0;
+    uint64_t black_queens = 0;
+    uint64_t black_king = 0;
+    uint64_t enpassant = 0;
 
-    bool can_black_castle_kingside;
-    bool can_black_castle_queenside;
-    bool can_white_castle_kindside;
-    bool can_white_castle_queenside;
+    bool white_to_play = false;
 
-    bool whiteToPlay;
+    bool can_black_castle_kingside = false;
+    bool can_black_castle_queenside = false;
+    bool can_white_castle_kindside = false;
+    bool can_white_castle_queenside = false;
+
+    int halfmove_clock = 0;
+    int fullmove_clock = 0;
 };
 
 namespace Initializers {
-    Position InitNewGameState();
-}
+    Position StartingPosition();
+    Position PositionFromFenString(std::string fen_string);
+}  // namespace Initializers
 
 namespace Output {
-    void PrintGamestate(Gamestate::Position position);
+    void PrintPosition(Gamestate::Position position);
 }  // namespace Output
 
 namespace Helpers {
-    bool HasPieceAtBinaryLocation(uint64_t binary_location, uint64_t binary_position);
+    bool PieceUint64HasPieceAtLocation(uint64_t piece_64bit_locations, uint64_t location_to_check);
 }  // namespace Helpers
 }  // namespace Gamestate
 
